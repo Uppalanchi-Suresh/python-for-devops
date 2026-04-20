ca88953b8b2f8de14cf565f4152c0d073e149ff4
Suresh Uppalanchi <uppalanchi.04@gmail.com>
2026-04-20 16:22:32 +0530

    Def function example

--- Day-02/examples/03-regex-replace.py
+++ Day-02/examples/03-regex-replace.py
@@ -1,9 +1,9 @@
 import re
 
 text = "The quick brown fox jumps over the lazy brown dog"
-pattern = r"brown"
+pattern = "brown"
 
-replacement = "red"
+replacement = "dark blue"
 
 new_text = re.sub(pattern, replacement, text)
 print("Modified text:", new_text)
---
+++ Day-04/function.py
@@ -0,0 +1,18 @@
+
+name = "Beyoncé"
+name1 = "Hell"
+
+def greet(name):
+    return f"Hello {name},"
+
+def hell(name1):
+    return f"Go to {name1}!"
+
+print(greet("Beyonce") + " " + hell("Hell"))
+
+    #print(message)
+
+
+
+
+
