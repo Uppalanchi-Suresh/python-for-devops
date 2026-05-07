be5ea910dbcdf33311fcc7f57e4ada356c88bcfd
Suresh Uppalanchi <uppalanchi.04@gmail.com>
2026-05-07 13:54:19 +0530

    removed secrets

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -6,7 +6,7 @@ import json
 
 url = "https://uppalanchi.atlassian.net/rest/api/3/project"
 
-API_TOKEN="ATCTT3xFfGN0EW3DOHV6X0SlQuL52skL1PpZ1m8YT0Z58-bTAfiX4Slm21GsseQOnjlnKKqsP5JgOpoLXHS--IJfDZNLdM1PKDDu0K2DePXjj0R5cxZFWP6HkcHV1s_R3ryr-dibBaxhXzFd_NozkkSeBuj3U11BjbVGJJlp2H7NtBESqk86yaI=F96AF31D"
+API_TOKEN=""
 
 auth = HTTPBasicAuth("", API_TOKEN)
 
